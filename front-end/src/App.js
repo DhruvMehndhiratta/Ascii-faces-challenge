e72c87b603a48f14b2e3585cf87e7c4f382a052b
Dhruv Mehndhiratta <dmrrattaa7@gmail.com>
2019-12-20 10:40:29 +0530

    worked on loaders

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,35 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Row, Col, Container } from 'react-bootstrap';
-
+import loader from './assets/images/loader.gif'
 
 class App extends Component {
   state = {
-    faces: []
+    faces: [],
+    loading:false
   }
   componentDidMount() {
+    this.setState( {loading:true})
     fetch('http://localhost:3000/products').then(resp => resp.json()).then(faces => {
       this.setState({
-        faces
+        faces,
+        loading:false
       })
-    })
+    }).catch(err =>  this.setState({ loading: false }))
   }
 
   render() {
-    const { faces = [] } = this.state;
-    console.log(faces, "faces")
+    const { faces = [], loading } = this.state;
     return (
       <Container>
         <Row className='m-0'>
           {
-            faces.length ? faces.map((item, i) => {
+           !loading ?  faces.length ? faces.map((item, i) => {
               return <Col sm={4} md={3} lg={2} xs={6} key={i}>
                 <div className='demo'>
                   <div className='overlay' style={{ fontSize: `${item.size} px` }}>{item.face}</div>
                   <h6 className='text-center  my-2'>{`$${item.price}`}</h6>
                 </div>
               </Col>
-            }) : null
+            }) : null :  <React.Fragment>  {
+                  [...Array(500)].map((item)=> {
+                    return <Col sm={4} md={3} lg={2} xs={6}>
+                    <div className='demo border-0'>
+                      <img style={{ height:"50px" , width:"50px"}} src={loader}/>
+                    </div>
+                  </Col>
+                  })
+                 
+                }
+                </React.Fragment>
+
+            
+
           }
         </Row>
       </Container>
